5237256b9fa268df5d9692df024e706f3b709354
caiocerruti <caio.goncalves@dextra-sw.com>
2021-02-01 16:40:40 -0300

    refactor: Alteraçao do background quadrado antigo para o background retangular novo. Para tal foi alterada a imagem fa chamada de background no diretorio atlases para outra de mesmo nome. Alem disso, alterou-se os valores de GAME_WIDTH e GAME_HEIGHT em dextra_quario.dart, bem como a primeira linha do metodo de calcular a escala.

--- lib/dextra_quario.dart
+++ lib/dextra_quario.dart
@@ -12,8 +12,8 @@ import './components/fish.dart';
 import './components/bubble_source.dart';
 
 class DextraQuario extends BaseGame with TapDetector {
-  static const GAME_WIDTH = 320;
-  static const GAME_HEIGHT = 320;
+  static const GAME_WIDTH = 1440;
+  static const GAME_HEIGHT = 900;
 
   double _scaleFactor;
   Vector2 _translateFactor;
@@ -42,7 +42,7 @@ class DextraQuario extends BaseGame with TapDetector {
 
   void _calcScaleFactor() {
     // We can use either width or height since the resolution is a square one
-    final _scaleRaw = (min(size.x, size.y) / GAME_WIDTH);
+    final _scaleRaw = (min(size.x, size.y) / (min(GAME_WIDTH, GAME_HEIGHT)));
     _scaleFactor = _scaleRaw - _scaleRaw % 0.02;
 
     final _finalWidth = _scaleFactor * GAME_WIDTH;
