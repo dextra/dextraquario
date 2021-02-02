d757c3b3ea2ce3bcc6e2402d45e9f4a3d0678ab4
Alan Souza <alan.souza@dextra-sw.com>
2021-02-02 16:03:35 -0300

    feat: metodo update contribution approval status

--- lib/services/contribution_service.dart
+++ lib/services/contribution_service.dart
@@ -35,6 +35,12 @@ class ContributionServices {
     });
   }
 
+  // Get contribution by id
+  Future<ContributionModel> getContributionById(String id) =>
+      firebaseFirestore.collection(collection).doc(id).get().then((doc) {
+        return ContributionModel.fromSnapshot(doc);
+      });
+
   // Get contribution by user
   Future<ContributionModel> getContributionByUser(String id) =>
       firebaseFirestore.collection(collection).doc(id).get().then((doc) {
@@ -56,5 +62,20 @@ class ContributionServices {
     });
 
     return contributions;
+  }
+
+  Future updateContributionApproval(String contribution_id, bool approved) async {
+    String approvalStatus;
+
+    if (approved) {
+      approvalStatus = ApprovalStatus.APPROVED.toString().split('.').last;
+    } else {
+      approvalStatus = ApprovalStatus.DENIED.toString().split('.').last;
+    }
+
+    //update
+    firebaseFirestore.collection(collection).doc(contribution_id).set({
+      'approval': approvalStatus,
+    });
   }
 }
