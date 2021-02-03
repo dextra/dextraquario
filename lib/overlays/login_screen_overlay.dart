7c3ee26ce368f3f61c96066098d2fa2c5cb89443
thomaspl1 <thomas.passos@yahoo.com.br>
2021-02-03 13:31:07 -0300

    refactor: correct shadow of some components

--- lib/overlays/login_screen_overlay.dart
+++ lib/overlays/login_screen_overlay.dart
@@ -24,8 +24,8 @@ class LoginScreenOverlay extends StatelessWidget {
           Stack(
             children: [
               Container(
-                child:
-                    Image.asset('images/fish_green.png', color: Colors.black),
+                child: Image.asset('images/fish_green.png',
+                    color: Color.fromRGBO(0, 0, 0, 0.75)),
                 padding: EdgeInsets.only(top: 2.0, left: 2.0),
               ),
               ClipRect(
@@ -46,7 +46,7 @@ class LoginScreenOverlay extends StatelessWidget {
                   Shadow(
                     blurRadius: 0,
                     offset: Offset(4.0, 4.0),
-                    color: Color.fromARGB(255, 0, 0, 0),
+                    color: Color.fromRGBO(0, 0, 0, 0.75),
                   ),
                 ],
               ),
@@ -58,49 +58,76 @@ class LoginScreenOverlay extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               GestureDetector(
-                child: NineTileBox(
-                  image: Assets.buttonImage,
-                  tileSize: 12,
-                  destTileSize: 16,
-                  width: 264,
-                  height: 48,
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                            child: Text(
-                              'Login com o',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      blurRadius: 0,
-                                      offset: Offset(1.0, 1.0),
-                                      color: Color.fromARGB(255, 0, 0, 0))
-                                ],
-                              ),
-                            ),
-                            padding: EdgeInsets.only(right: 16.0)),
-                        Stack(
-                          children: [
-                            Container(
-                              child: Image.asset('images/google_logo.png',
-                                  color: Colors.black),
-                              padding: EdgeInsets.only(top: 1.0, left: 1.0),
-                            ),
-                            ClipRect(
-                              child: BackdropFilter(
-                                filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-                                child: Image.asset('images/google_logo.png'),
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
+                child: Stack(
+                  children: [
+                    Container(
+                      child: Opacity(
+                        child: NineTileBox(
+                          image: Assets.buttonShadow,
+                          tileSize: 12,
+                          destTileSize: 16,
+                          width: 264,
+                          height: 48,
+                        ),
+                        opacity: 0.5,
+                      ),
+                      padding: EdgeInsets.only(top: 1.0, left: 1.0),
                     ),
-                  ),
+                    ClipRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+                        child: NineTileBox(
+                          image: Assets.buttonImage,
+                          tileSize: 12,
+                          destTileSize: 16,
+                          width: 264,
+                          height: 48,
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                    child: Text(
+                                      'Login com o',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                        shadows: <Shadow>[
+                                          Shadow(
+                                              blurRadius: 0,
+                                              offset: Offset(1.0, 1.0),
+                                              color:
+                                                  Color.fromRGBO(0, 0, 0, 0.75))
+                                        ],
+                                      ),
+                                    ),
+                                    padding: EdgeInsets.only(right: 16.0)),
+                                Stack(
+                                  children: [
+                                    Container(
+                                      child: Image.asset(
+                                          'images/google_logo.png',
+                                          color: Color.fromRGBO(0, 0, 0, 0.75)),
+                                      padding:
+                                          EdgeInsets.only(top: 1.0, left: 1.0),
+                                    ),
+                                    ClipRect(
+                                      child: BackdropFilter(
+                                        filter: ImageFilter.blur(
+                                            sigmaX: 0, sigmaY: 0),
+                                        child: Image.asset(
+                                            'images/google_logo.png'),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 onTap: () {
                   onClick?.call();
