436fb7127042407091229533d374c1ffe8e4c2b5
thomaspl1 <thomas.passos@yahoo.com.br>
2021-02-09 14:51:28 -0300

    refactor: make login screen resizable

--- lib/overlays/login_screen_overlay.dart
+++ lib/overlays/login_screen_overlay.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:dextraquario/utils/scale_factor_calculator.dart';
 import 'package:flame/widgets/nine_tile_box.dart';
 import 'package:flutter/material.dart';
 import 'package:dextraquario/providers/app.dart';
@@ -17,130 +18,151 @@ class LoginScreenOverlay extends StatelessWidget {
     final AuthProvider authProvider = Provider.of<AuthProvider>(context);
     final AppProvider appProvider = Provider.of<AppProvider>(context);
     appProvider.changeLoading();
-    return BackdropFilter(
-      filter: ImageFilter.blur(
-        sigmaX: 7,
-        sigmaY: 7,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              Container(
-                child: Image.asset('images/fish_green.png',
-                    color: Color.fromRGBO(0, 0, 0, 0.75)),
-                padding: EdgeInsets.only(top: 2.0, left: 2.0),
-              ),
-              ClipRect(
-                  child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-                      child: Image.asset('images/fish_green.png')))
-            ],
-          ),
-          Container(
-            child: Text(
-              'dextraquário',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 48,
-                height: 1.2,
-                shadows: <Shadow>[
-                  Shadow(
-                    blurRadius: 0,
-                    offset: Offset(4.0, 4.0),
+    return LayoutBuilder(builder: (context, constraints) {
+      var scaleFactor = ScaleFactorCalculator.calcScaleFactor(
+          constraints.maxWidth, constraints.maxHeight);
+
+      return BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 7,
+          sigmaY: 7,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  child: Image.asset(
+                    'images/fish_green.png',
                     color: Color.fromRGBO(0, 0, 0, 0.75),
+                    scale: 1.0 / scaleFactor,
                   ),
-                ],
-              ),
+                  padding: EdgeInsets.only(
+                      top: 2.0 * scaleFactor, left: 2.0 * scaleFactor),
+                ),
+                ClipRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+                    child: Image.asset('images/fish_green.png',
+                        scale: 1.0 / scaleFactor),
+                  ),
+                ),
+              ],
             ),
-            padding: EdgeInsets.only(bottom: 144),
-            transform: Matrix4.translationValues(0, -16, 0),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                child: Stack(
-                  children: [
-                    Container(
-                      child: Opacity(
-                        child: NineTileBox(
-                          image: Assets.buttonShadow,
-                          tileSize: 12,
-                          destTileSize: 16,
-                          width: 264,
-                          height: 48,
-                        ),
-                        opacity: 0.5,
-                      ),
-                      padding: EdgeInsets.only(top: 1.0, left: 1.0),
+            Container(
+              child: Text(
+                'dextraquário',
+                textScaleFactor: scaleFactor,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  height: 1.2,
+                  shadows: <Shadow>[
+                    Shadow(
+                      blurRadius: 0,
+                      offset: Offset(4.0 * scaleFactor, 4.0 * scaleFactor),
+                      color: Color.fromRGBO(0, 0, 0, 0.75),
                     ),
-                    ClipRect(
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-                        child: NineTileBox(
-                          image: Assets.buttonImage,
-                          tileSize: 12,
-                          destTileSize: 16,
-                          width: 264,
-                          height: 48,
-                          child: Center(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
+                  ],
+                ),
+              ),
+              padding: EdgeInsets.only(bottom: 144 * scaleFactor),
+              transform: Matrix4.translationValues(0, -16 * scaleFactor, 0),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  child: Stack(
+                    children: [
+                      Container(
+                        child: Opacity(
+                          child: NineTileBox(
+                            image: Assets.buttonShadow,
+                            tileSize: 12,
+                            destTileSize: 16,
+                            width: 264 * scaleFactor,
+                            height: 48 * scaleFactor,
+                          ),
+                          opacity: 0.5,
+                        ),
+                        padding: EdgeInsets.only(top: 1.0, left: 1.0),
+                      ),
+                      ClipRect(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+                          child: NineTileBox(
+                            image: Assets.buttonImage,
+                            tileSize: 12,
+                            destTileSize: 16,
+                            width: 264 * scaleFactor,
+                            height: 48 * scaleFactor,
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
                                     child: Text(
                                       'Login com o',
+                                      textScaleFactor: scaleFactor,
                                       style: TextStyle(
                                         color: Colors.white,
                                         fontSize: 14,
                                         shadows: <Shadow>[
                                           Shadow(
                                               blurRadius: 0,
-                                              offset: Offset(1.0, 1.0),
+                                              offset: Offset(1.0 * scaleFactor,
+                                                  1.0 * scaleFactor),
                                               color:
                                                   Color.fromRGBO(0, 0, 0, 0.75))
                                         ],
                                       ),
                                     ),
-                                    padding: EdgeInsets.only(right: 16.0)),
-                                Stack(
-                                  children: [
-                                    Container(
-                                      child: Image.asset(
-                                          'images/google_logo.png',
-                                          color: Color.fromRGBO(0, 0, 0, 0.75)),
-                                      padding:
-                                          EdgeInsets.only(top: 1.0, left: 1.0),
-                                    ),
-                                    ClipRect(
-                                      child: BackdropFilter(
-                                        filter: ImageFilter.blur(
-                                            sigmaX: 0, sigmaY: 0),
+                                    padding: EdgeInsets.only(
+                                        right: 16.0 * scaleFactor),
+                                  ),
+                                  Stack(
+                                    children: [
+                                      Container(
                                         child: Image.asset(
-                                            'images/google_logo.png'),
+                                          'images/google_logo.png',
+                                          scale: 1 / scaleFactor,
+                                          color: Color.fromRGBO(0, 0, 0, 0.75),
+                                        ),
+                                        padding: EdgeInsets.only(
+                                            top: 1.0 * scaleFactor,
+                                            left: 1.0 * scaleFactor),
                                       ),
-                                    ),
-                                  ],
-                                )
-                              ],
+                                      ClipRect(
+                                        child: BackdropFilter(
+                                          filter: ImageFilter.blur(
+                                              sigmaX: 0, sigmaY: 0),
+                                          child: Image.asset(
+                                              'images/google_logo.png',
+                                              scale: 1 / scaleFactor),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  onClick?.call();
-                },
-              )
-            ],
-          )
-        ],
-      ),
-    );
+                    ],
+                  ),
+                  onTap: () {
+                    onClick?.call();
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    });
   }
 }
