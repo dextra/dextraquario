46163a96da97ac46305ebe5a7f9c6e896a161f9a
Tyemy Kuga <tyemy.kuga@dextra-sw.com>
2021-02-11 22:02:13 -0300

    Firebase credentials

---
+++ config/init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwh4ZuWgc28hnZF7ej569xZ2_teswiH3w",
+    authDomain: "dextraquario.firebaseapp.com",
+    projectId: "dextraquario",
+    storageBucket: "dextraquario.appspot.com",
+    messagingSenderId: "494719801985",
+    appId: "1:494719801985:web:3c61b851b28d22b6ef4b80",
+    measurementId: "G-L4XK1CRH2L"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
