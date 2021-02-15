56be7292b3803b6dd1e07b3c620f2de10ad8bf81
IamN5 <painn5@yahoo.com>
2021-02-15 10:32:06 -0300

    fix: Correctly display user ranking on FishOverlay

--- lib/overlays/fish_overlay.dart
+++ lib/overlays/fish_overlay.dart
@@ -4,31 +4,33 @@ import 'package:dextraquario/services/contribution_service.dart';
 import 'package:dextraquario/services/user_service.dart';
 import 'package:flutter/material.dart';
 
-class FishOverlay extends StatelessWidget {  
+class FishOverlay extends StatelessWidget {
   final FishInfo fishInfo;
   final Function onCloseInfo;
+  final UserServices _userServices = UserServices();
 
   FishOverlay({this.fishInfo, this.onCloseInfo});
 
   @override
-  Widget build(context) {    
+  Widget build(context) {
     return FutureBuilder(
       future: Future.wait([
-        UserServices().getUserById(fishInfo.userID), 
+        _userServices.getUserById(fishInfo.userID),
         ContributionServices().getContributionsByUser(fishInfo.userID),
+        _userServices.getUserPlacement(fishInfo.userID)
       ]),
-      builder: (ctx, snapshot){        
-        if(snapshot.hasData){        
+      builder: (ctx, snapshot) {
+        if (snapshot.hasData) {
           return ProfileScreen(
             onClose: onCloseInfo,
             user: snapshot.data[0],
             contributions: snapshot.data[1],
-            userRanking: 1,
+            userRanking: snapshot.data[2],
           );
         }
 
         return Container();
-      },                            
+      },
     );
   }
 }
